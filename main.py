ec838b8951125c0e7e74760ee782183e66d463bc
devshivamthakur <shivamthakurcool01@gmail.com>
2026-01-22 17:21:31 +0530

    Refactor FastAPI initialization to set openapi_url to None

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 
-app = FastAPI()
+app = FastAPI(openapi_url=None)
 limiter = Limiter(key_func=get_remote_address)
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
-
 
 origins = [
    "*"
